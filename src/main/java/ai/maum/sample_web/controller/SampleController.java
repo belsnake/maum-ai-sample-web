999415f98b025d9cc63360910eec6c6221a64f0f
belsnake <belsnake@mindslab.ai>
2019-12-11 17:53:02 +0900

    * 모든 HTML에 타임리프 적용 * 영어교육 STT에 API ID/KEY 보안 적용

--- src/main/java/ai/maum/sample_web/controller/SampleController.java
+++ src/main/java/ai/maum/sample_web/controller/SampleController.java
@@ -3,8 +3,10 @@ package ai.maum.sample_web.controller;
 import ai.maum.sample_web.service.SampleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +23,12 @@ public class SampleController {
     @Autowired
     SampleService sampleService;
 
+    @Value("${api.id}")
+    private String API_ID;
+
+    @Value("${api.key}")
+    private String API_KEY;
+
     /*
     ** HOME
     */
@@ -29,4 +37,38 @@ public class SampleController {
         return "home";
     }
 
+    /*
+     ** MRC
+     */
+    @RequestMapping(value = "/sampleMrc")
+    public String sampleMrc() throws IOException {
+        return "sampleMrc";
+    }
+
+    /*
+     ** TTS
+     */
+    @RequestMapping(value = "/sampleTts")
+    public String sampleTts() throws IOException {
+        return "sampleTts";
+    }
+
+    /*
+     ** FileSTT
+     */
+    @RequestMapping(value = "/sampleFileStt")
+    public String sampleFileStt() throws IOException {
+        return "sampleFileStt";
+    }
+
+    /*
+     ** EngEduSTT
+     */
+    @RequestMapping(value = "/sampleEngEduStt")
+    public String sampleEngEduStt(Model model) throws IOException {
+
+        model.addAttribute("apiId", API_ID);
+        model.addAttribute("apiKey", API_KEY);
+        return "sampleEngEduStt";
+    }
 }
